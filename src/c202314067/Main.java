c84b97a37d6459ade291d3fd0abb0b2618b6e8ba
Choi, Seungil <jschoi020102@gmail.com>
2025-04-22 23:38:02 +0900

    five

--- src/c202314067/Main.java
+++ src/c202314067/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("최승일");
 		World.run();
+		World.run2();
 	}
+	
 
 }
---
+++ src/c202314067/World.java
@@ -0,0 +1,10 @@
+package c202314067;
+
+public class World {
+	public static void run() {
+		System.out.println("202314067");
+	}
+	public static void run2() {
+		System.out.println("hello world");
+	}
+}
